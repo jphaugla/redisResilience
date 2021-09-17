7b3074d7d32b0532ba1b6cfcbcb17a21a53a99e6
Jason Haugland <jason@Jasons-MacBook-Pro.local>
2021-09-17 15:38:58 -0500

    missed file

--- src/main/java/com/jphaugla/repository/CustomerRepository.java
+++ src/main/java/com/jphaugla/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jphaugla.domain.Customer;
 import com.jphaugla.service.ChooseRedis;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.HashOperations;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 @Repository
@@ -32,7 +37,7 @@ public class CustomerRepository {
 
 	@Autowired
 	@Qualifier("redisTemplateReadArray")
-	private RedisTemplate[] redisTemplateReadArray;
+	private StringRedisTemplate[] redisTemplateReadArray;
 
 	ObjectMapper mapper = new ObjectMapper();
 
@@ -40,12 +45,15 @@ public class CustomerRepository {
 		logger.info("CustomerRepository constructor");
 	}
 
-	@CircuitBreaker(name = "backendA", fallbackMethod = "createCallBack")
+	@Retry(name = "backendA")
 	public String create(Customer customer) {
+		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+		Long millis = System.currentTimeMillis();
+		Date date = new Date(millis);
+		logger.info("in customerrepository.create with customerID " + customer.getCustomerId() + " redisIndex is " + chooseRedis.getRedisIndex() + " " + date);
 		if (customer.getCreatedDatetime() == null) {
-			Long currentTimeMillis = System.currentTimeMillis();
-			customer.setCreatedDatetime(currentTimeMillis);
-			customer.setLastUpdated(currentTimeMillis);
+			customer.setCreatedDatetime(millis);
+			customer.setLastUpdated(millis);
 		}
 		Map<Object, Customer> customerHash = mapper.convertValue(customer, Map.class);
 		redisTemplateWriteArray[chooseRedis.getRedisIndex()].opsForHash().putAll("Customer:" + customer.getCustomerId(), customerHash);
@@ -53,16 +61,16 @@ public class CustomerRepository {
 		return "Success\n";
 	}
 
-	@CircuitBreaker(name = "backendA", fallbackMethod = "getCallBack")
+	@Retry(name = "backendA")
 	public Customer get(String customerId) {
 		logger.info("in CustomerRepository.get with customer id=" + customerId);
 		String fullKey = "Customer:" + customerId;
-		Map<Object, Customer> customerHash = redisTemplateReadArray[chooseRedis.getRedisIndex()].opsForHash().entries(fullKey);
+		Map<Object, Object> customerHash = redisTemplateReadArray[chooseRedis.getRedisIndex()].opsForHash().entries(fullKey);
 		// Map<Object, Object> customerHash = redisTemplate1.opsForHash().entries(fullKey);
 		Customer customer = mapper.convertValue(customerHash, Customer.class);
 		return (customer);
 	}
-
+	// likely, not needed
 	public String createCallBack(Customer customer, Exception exception) {
 		//  this gets called back with every exception but only do the switch
 		//  when it is called by the called not permitted exception (circuit breaker open)
