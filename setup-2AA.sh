c1aa28ff403a91083d7362d1fe22f74aa06f19d2
Jason Haugland <jasonhaugland@gmail.com>
2022-04-12 16:47:16 -0500

    drop down to one node per cluster and add java VM and Dockerfile

--- setup-2AA.sh
+++ setup-2AA.sh
@@ -20,8 +20,8 @@ printf ' ready!\n'
 printf '\nnode 1: '
 docker exec -it re1 rladmin cluster create name cluster-1.redis-enterprise.com username $ADMIN_U password $ADMIN_P
 
-printf '\nnode 2: '
-docker exec -it re2 rladmin cluster join nodes 172.22.0.11 username $ADMIN_U password $ADMIN_P
+# printf '\nnode 2: '
+# docker exec -it re2 rladmin cluster join nodes 172.22.0.11 username $ADMIN_U password $ADMIN_P
 
 printf '\nCluster 1 setup complete!\n'
 
@@ -34,17 +34,17 @@ printf ' ready!\n'
 printf '\nnode 3: '
 docker exec -it re3 rladmin cluster create name cluster-2.redis-enterprise.com username $ADMIN_U password $ADMIN_P
 
-printf '\nnode 4: '
-docker exec -it re4 rladmin cluster join nodes 172.22.0.13 username $ADMIN_U password $ADMIN_P
+# printf '\nnode 4: '
+# docker exec -it re4 rladmin cluster join nodes 172.22.0.13 username $ADMIN_U password $ADMIN_P
 
 printf '\nCluster 2 setup complete!\n'
 
 
 printf "\n\nCreating IP routes - wait for updates..."
 docker exec --user root re1 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300  >/dev/null"
-docker exec --user root re2 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300  >/dev/null"
+# docker exec --user root re2 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300  >/dev/null"
 docker exec --user root re3 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300  >/dev/null"
-docker exec --user root re4 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300  >/dev/null"
+# docker exec --user root re4 bash -c "iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300  >/dev/null"
 sleep 60
 echo ok
 
---
+++ src/main/resources/runApplication.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar /usr/app/redis-0.0.1-SNAPSHOT.jar
